b1468d64e76ab90095b2dcb7667053ae5cc80eb8
Bogdan <sinitsa.bogdan.1997@yandex.ru>
2024-09-18 17:39:40 +0300

    feat(arrowbutton): добовление нофой функциональности

--- src/components/arrow-button/ui/ArrowButton.tsx
+++ src/components/arrow-button/ui/ArrowButton.tsx
@@ -5,15 +5,26 @@ import styles from '../styles/ArrowButton.module.scss';
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+export const ArrowButton = ({
+	isOpen = false,
+	onClick,
+}: {
+	isOpen?: boolean;
+	onClick?: OnClick;
+}) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={`${styles.container} ${isOpen && styles.container_open}`}
+			onClick={onClick}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={`${styles.arrow} ${isOpen && styles.arrow_open}`}
+			/>
 		</div>
 	);
 };
