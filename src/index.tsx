cf6af346c433371cad56cf106653e2d691981633
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-09-18 21:32:04 +0300

    feat(index.tsx): реализация основного функционала приложения

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState, useRef, useEffect } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/ui/Article';
@@ -13,19 +13,64 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const dom = useRef<HTMLDivElement | null>(null);
+	const [isClear, setClear] = useState(false);
+	const [articleState, setArticleState] = useState(defaultArticleState);
+
+	useEffect(() => {
+		if (isClear) {
+			handlerOnClickToSave();
+			setClear(false);
+		}
+	});
+
+	function handlerOnClickToClear(): void {
+		setArticleState(defaultArticleState);
+		setClear(true);
+	}
+
+	function handlerOnClickToSave(): void {
+		dom.current?.style.setProperty(
+			'--font-family',
+			articleState.fontFamilyOption.value
+		);
+		dom.current?.style.setProperty(
+			'--font-size',
+			articleState.fontSizeOption.value
+		);
+		dom.current?.style.setProperty(
+			'--font-color',
+			articleState.fontColorOption.value
+		);
+		dom.current?.style.setProperty(
+			'--container-width',
+			articleState.contentWidthOption.value
+		);
+		dom.current?.style.setProperty(
+			'--bg-color',
+			articleState.backgroundColorOption.value
+		);
+	}
+
 	return (
 		<div
 			className={clsx(styles.main)}
+			ref={dom}
 			style={
 				{
 					'--font-family': defaultArticleState.fontFamilyOption.value,
 					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-color': defaultArticleState.fontColorOption.value,
+					'--container-width': defaultArticleState.contentWidthOption.value,
+					'--bg-color': defaultArticleState.backgroundColorOption.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				state={articleState}
+				setState={setArticleState}
+				clear={handlerOnClickToClear}
+				save={handlerOnClickToSave}
+			/>
 			<Article />
 		</div>
 	);
