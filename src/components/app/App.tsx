cb7829f3dc81823dc31fb3423273e2ac208a1f65
Синица Богдан <sinitsa.bogdan.1997@yandex.ru>
2024-09-19 15:33:14 +0300

    refactor: изменение логики обновления состояния приложения

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties, useEffect, useRef, useState } from 'react';
+import { CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { defaultArticleState } from 'src/constants/articleProps';
@@ -10,64 +10,21 @@ import styles from './styles/index.module.scss';
 import './styles/index.scss';
 
 export const App = () => {
-	const dom = useRef<HTMLDivElement | null>(null);
-	const [isClear, setClear] = useState(false);
-	const [articleState, setArticleState] = useState(defaultArticleState);
-
-	useEffect(() => {
-		if (isClear) {
-			handlerOnClickToSave();
-			setClear(false);
-		}
-	});
-
-	function handlerOnClickToClear(): void {
-		setArticleState(defaultArticleState);
-		setClear(true);
-	}
-
-	function handlerOnClickToSave(): void {
-		dom.current?.style.setProperty(
-			'--font-family',
-			articleState.fontFamilyOption.value
-		);
-		dom.current?.style.setProperty(
-			'--font-size',
-			articleState.fontSizeOption.value
-		);
-		dom.current?.style.setProperty(
-			'--font-color',
-			articleState.fontColorOption.value
-		);
-		dom.current?.style.setProperty(
-			'--container-width',
-			articleState.contentWidthOption.value
-		);
-		dom.current?.style.setProperty(
-			'--bg-color',
-			articleState.backgroundColorOption.value
-		);
-	}
+	const [appState, setAppState] = useState(defaultArticleState);
 
 	return (
 		<div
 			className={clsx(styles.main)}
-			ref={dom}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColorOption.value,
-					'--container-width': defaultArticleState.contentWidthOption.value,
-					'--bg-color': defaultArticleState.backgroundColorOption.value,
+					'--font-family': appState.fontFamilyOption.value,
+					'--font-size': appState.fontSizeOption.value,
+					'--font-color': appState.fontColorOption.value,
+					'--container-width': appState.contentWidthOption.value,
+					'--bg-color': appState.backgroundColorOption.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm
-				state={articleState}
-				setState={setArticleState}
-				clear={handlerOnClickToClear}
-				save={handlerOnClickToSave}
-			/>
+			<ArticleParamsForm updateAppState={setAppState} />
 			<Article />
 		</div>
 	);
